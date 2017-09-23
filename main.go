721d8216729011f79892c20b556bb556f2007c76
Amarantz <se0022@hotmail.com>
2017-09-23 12:49:16 -0600

    Added imports

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package web_fire_family
 
-func main() {
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+
+func main() {
+ router := mux.NewRouter()
+ log.Fatal(http.ListenAndServe(":8000", router))
 }
