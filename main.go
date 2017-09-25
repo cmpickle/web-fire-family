7454c0df16b0695928209d0cb0f853ab6607b32f
Amarantz <se0022@hotmail.com>
2017-09-25 17:43:01 -0600

    Added Temp Routs to file. Added comments for the routs.

--- main.go
+++ main.go
@@ -11,5 +11,15 @@ import (
 
 func main() {
  router := mux.NewRouter()
+ //This should bring a list of all the Products
+ router.HandleFunc("/product", GetProducts).Methods("GET")
+ // This should bring back a specific Product
+ router.HandleFunc("/product/{id}", GetProduct).Methods("GET")
+ //This creates a new product using a Json String
+ router.HandleFunc("/product/Create",CreateProduct).Methods("POST")
+ //This sets the product to inactive in the database
+ router.HandleFunc("/product/delete/{id}", DeleteProduct).Methods("DELETE")
+ //This allows us to set the quantity value of a product.
+ router.HandleFunc("/inventory/update/{id}/{quantity}",UpdateInventory).Methods("POST")
  log.Fatal(http.ListenAndServe(":8000", router))
 }
