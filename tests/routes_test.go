ca0ec82ef455192f1970dcb83d351036df4cb79b
Cameron Pickle <cmpickle@gmail.com>
2017-10-17 21:44:35 -0600

    added unit tests for deleteProduct and fixed other routes's unit tests

--- tests/routes_test.go
+++ tests/routes_test.go
@@ -8,7 +8,7 @@ import (
 	"reflect"
 	"testing"
 
-	"../routes"
+	"github.com/Xero67/web-fire-family/routes"
 )
 
 func TestGetProducts(t *testing.T) {
@@ -20,13 +20,15 @@ func TestGetProducts(t *testing.T) {
 
 	w := httptest.NewRecorder()
 
-	routes.InitRoutes().ServeHTTP(w, req)
+	router := routes.InitRoutes()
 
 	routes.Products = nil
 
 	routes.Products = append(routes.Products, routes.Product{ProductID: 1, ProductName: "Firefighter Wallet", InventoryScanningID: 1, Color: "Tan", Price: 30, Dimensions: "3 1/2\" tall and 4 1/2\" long", SKU: 1})
 	routes.Products = append(routes.Products, routes.Product{ProductID: 2, ProductName: "Firefighter Apron", InventoryScanningID: 2, Color: "Tan", Size: "One Size Fits All", Price: 29, Dimensions: "31\" tall and 26\" wide and ties around a waist up to 54\"", SKU: 2})
 	routes.Products = append(routes.Products, routes.Product{ProductID: 3, ProductName: "Firefighter Baby Outfit", InventoryScanningID: 3, Color: "Tan", Size: "Newborn", Price: 39.99, Dimensions: "Waist-14\", Length-10\"", SKU: 3})
+
+	router.ServeHTTP(w, req)
 
 	// Check the status code is what we expect.
 	if status := w.Code; status != http.StatusOK {
@@ -50,13 +52,15 @@ func TestGetProduct(t *testing.T) {
 
 	w := httptest.NewRecorder()
 
-	routes.InitRoutes().ServeHTTP(w, req)
+	router := routes.InitRoutes()
 
 	routes.Products = nil
 
 	routes.Products = append(routes.Products, routes.Product{ProductID: 1, ProductName: "Firefighter Wallet", InventoryScanningID: 1, Color: "Tan", Price: 30, Dimensions: "3 1/2\" tall and 4 1/2\" long", SKU: 1})
 	routes.Products = append(routes.Products, routes.Product{ProductID: 2, ProductName: "Firefighter Apron", InventoryScanningID: 2, Color: "Tan", Size: "One Size Fits All", Price: 29, Dimensions: "31\" tall and 26\" wide and ties around a waist up to 54\"", SKU: 2})
 	routes.Products = append(routes.Products, routes.Product{ProductID: 3, ProductName: "Firefighter Baby Outfit", InventoryScanningID: 3, Color: "Tan", Size: "Newborn", Price: 39.99, Dimensions: "Waist-14\", Length-10\"", SKU: 3})
+
+	router.ServeHTTP(w, req)
 
 	// Check the status code is what we expect.
 	if status := w.Code; status != http.StatusOK {
@@ -80,13 +84,15 @@ func TestGetProductInvalidID(t *testing.T) {
 
 	w := httptest.NewRecorder()
 
-	routes.InitRoutes().ServeHTTP(w, req)
+	router := routes.InitRoutes()
 
 	routes.Products = nil
 
 	routes.Products = append(routes.Products, routes.Product{ProductID: 1, ProductName: "Firefighter Wallet", InventoryScanningID: 1, Color: "Tan", Price: 30, Dimensions: "3 1/2\" tall and 4 1/2\" long", SKU: 1})
 	routes.Products = append(routes.Products, routes.Product{ProductID: 2, ProductName: "Firefighter Apron", InventoryScanningID: 2, Color: "Tan", Size: "One Size Fits All", Price: 29, Dimensions: "31\" tall and 26\" wide and ties around a waist up to 54\"", SKU: 2})
 	routes.Products = append(routes.Products, routes.Product{ProductID: 3, ProductName: "Firefighter Baby Outfit", InventoryScanningID: 3, Color: "Tan", Size: "Newborn", Price: 39.99, Dimensions: "Waist-14\", Length-10\"", SKU: 3})
+
+	router.ServeHTTP(w, req)
 
 	// Check the status code is what we expect.
 	if status := w.Code; status != http.StatusOK {
@@ -110,13 +116,15 @@ func TestGetProductNegativeID(t *testing.T) {
 
 	w := httptest.NewRecorder()
 
-	routes.InitRoutes().ServeHTTP(w, req)
+	router := routes.InitRoutes()
 
 	routes.Products = nil
 
 	routes.Products = append(routes.Products, routes.Product{ProductID: 1, ProductName: "Firefighter Wallet", InventoryScanningID: 1, Color: "Tan", Price: 30, Dimensions: "3 1/2\" tall and 4 1/2\" long", SKU: 1})
 	routes.Products = append(routes.Products, routes.Product{ProductID: 2, ProductName: "Firefighter Apron", InventoryScanningID: 2, Color: "Tan", Size: "One Size Fits All", Price: 29, Dimensions: "31\" tall and 26\" wide and ties around a waist up to 54\"", SKU: 2})
 	routes.Products = append(routes.Products, routes.Product{ProductID: 3, ProductName: "Firefighter Baby Outfit", InventoryScanningID: 3, Color: "Tan", Size: "Newborn", Price: 39.99, Dimensions: "Waist-14\", Length-10\"", SKU: 3})
+
+	router.ServeHTTP(w, req)
 
 	// Check the status code is what we expect.
 	if status := w.Code; status != http.StatusOK {
@@ -128,6 +136,88 @@ func TestGetProductNegativeID(t *testing.T) {
 	equal, err := AreEqualJSON(w.Body.String(), expected)
 	if !equal {
 		t.Errorf("handler returned unexpected body: got %v want %v", w.Body.String(), expected)
+	}
+}
+
+func TestDeleteProduct(t *testing.T) {
+	// Create a request to pass to our handler. We don't have any query parameters for now so we'll pass 'nil' as the third parameter.
+	req, err := http.NewRequest("DELETE", "/product/delete/2", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req2, err := http.NewRequest("GET", "/product", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+
+	w2 := httptest.NewRecorder()
+
+	router := routes.InitRoutes()
+
+	routes.Products = nil
+
+	routes.Products = append(routes.Products, routes.Product{ProductID: 1, ProductName: "Firefighter Wallet", InventoryScanningID: 1, Color: "Tan", Price: 30, Dimensions: "3 1/2\" tall and 4 1/2\" long", SKU: 1})
+	routes.Products = append(routes.Products, routes.Product{ProductID: 2, ProductName: "Firefighter Apron", InventoryScanningID: 2, Color: "Tan", Size: "One Size Fits All", Price: 29, Dimensions: "31\" tall and 26\" wide and ties around a waist up to 54\"", SKU: 2})
+	routes.Products = append(routes.Products, routes.Product{ProductID: 3, ProductName: "Firefighter Baby Outfit", InventoryScanningID: 3, Color: "Tan", Size: "Newborn", Price: 39.99, Dimensions: "Waist-14\", Length-10\"", SKU: 3})
+
+	router.ServeHTTP(w, req)
+
+	router.ServeHTTP(w2, req2)
+
+	// Check the status code is what we expect.
+	if status := w.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	expected := `[{"productid":1,"productname":"Firefighter Wallet","inventoryscanningid":1,"color":"Tan","price":30,"dimensions":"3 1/2\" tall and 4 1/2\" long","sku":1},{"productid":3,"productname":"Firefighter Baby Outfit","inventoryscanningid":3,"color":"Tan","size":"Newborn","price":39.99,"dimensions":"Waist-14\", Length-10\"","sku":3}]`
+	equal, err := AreEqualJSON(w2.Body.String(), expected)
+	if !equal {
+		t.Errorf("handler returned unexpected body: got %v want %v", w2.Body.String(), expected)
+	}
+}
+
+func TestDeleteProductNonExistant(t *testing.T) {
+	// Create a request to pass to our handler. We don't have any query parameters for now so we'll pass 'nil' as the third parameter.
+	req, err := http.NewRequest("DELETE", "/product/delete/8", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req2, err := http.NewRequest("GET", "/product", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+
+	w2 := httptest.NewRecorder()
+
+	router := routes.InitRoutes()
+
+	routes.Products = nil
+
+	routes.Products = append(routes.Products, routes.Product{ProductID: 1, ProductName: "Firefighter Wallet", InventoryScanningID: 1, Color: "Tan", Price: 30, Dimensions: "3 1/2\" tall and 4 1/2\" long", SKU: 1})
+	routes.Products = append(routes.Products, routes.Product{ProductID: 2, ProductName: "Firefighter Apron", InventoryScanningID: 2, Color: "Tan", Size: "One Size Fits All", Price: 29, Dimensions: "31\" tall and 26\" wide and ties around a waist up to 54\"", SKU: 2})
+	routes.Products = append(routes.Products, routes.Product{ProductID: 3, ProductName: "Firefighter Baby Outfit", InventoryScanningID: 3, Color: "Tan", Size: "Newborn", Price: 39.99, Dimensions: "Waist-14\", Length-10\"", SKU: 3})
+
+	router.ServeHTTP(w, req)
+
+	router.ServeHTTP(w2, req2)
+
+	// Check the status code is what we expect.
+	if status := w.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	expected := `[{"productid":1,"productname":"Firefighter Wallet","inventoryscanningid":1,"color":"Tan","price":30,"dimensions":"3 1/2\" tall and 4 1/2\" long","sku":1},{"productid":2,"productname":"Firefighter Apron","inventoryscanningid":2,"color":"Tan","size":"One Size Fits All","price":29,"dimensions":"31\" tall and 26\" wide and ties around a waist up to 54\"","sku":2},{"productid":3,"productname":"Firefighter Baby Outfit","inventoryscanningid":3,"color":"Tan","size":"Newborn","price":39.99,"dimensions":"Waist-14\", Length-10\"","sku":3}]`
+	equal, err := AreEqualJSON(w2.Body.String(), expected)
+	if !equal {
+		t.Errorf("handler returned unexpected body: got %v want %v", w2.Body.String(), expected)
 	}
 }
 
