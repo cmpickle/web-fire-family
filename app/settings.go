4bb14c6e5f783beb98c331b35b19b510c1b96752
Amarantz <se0022@hotmail.com>
2017-10-23 20:38:39 -0600

    I break everything God damn it

--- app/settings.go
+++ app/settings.go
@@ -2,22 +2,30 @@ package app
 
 import (
 	"gopkg.in/yaml.v2"
-	"fmt"
+	//"fmt"
 	"log"
+	//"io"
+	"io/ioutil"
+	//"os"
 )
 
 type database struct{
-	y string `mysql:",inline"`
+	y string `yaml:"mysql,inline"`
 }
 
 type driver struct {
-	database `mysql:",inline"`
-	driver string `driver:"a,omitempty"`
-	host string `host:"a,omitempty"`
-	dbuser string `dbuser:"a,omitempty"`
-	dbpass string `dbpass:"a,omitempty`
-	port int `port:a,omitempty`
+	database database `yaml:"database,inline"`
+	driver string `yaml:"driver,omitempty"`
+	host string `yaml:"host,omitempty"`
+	dbuser string `yaml:"user,omitempty"`
+	dbpass string `yaml:"pass,omitempty"`
+	port int `port:"port,omitempty"`
 }
-
-
-
+var dbdriver driver
+func loadSettings() {
+	dat, err := ioutil.ReadFile("../config.yml")
+	yaml.Unmarshal(dat, &dbdriver)
+	if err != nil {
+		log.Fatal("cannot unmarshal data %v", err)
+	}
+}
