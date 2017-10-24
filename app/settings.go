3e12659b65d710b2635eb98961dc3b91e0618d5e
Amarantz <se0022@hotmail.com>
2017-10-24 17:23:58 -0600

    I break everything God damn it Changed the settings to be in the routes file Update yaml import to include web settings Need to figure out why I cannot get my app settings import to work.

--- app/settings.go
+++ app/settings.go
@@ -9,11 +9,20 @@ import (
 	//"os"
 )
 
+type web  struct {
+	y string `yaml:"web,inline"`
+}
+
+type webSettings struct {
+	port int `port:"port",omitempty`
+}
+
 type database struct{
+	// should move to the inline of the file. so we get whats under the mysql structure of the yaml file.
 	y string `yaml:"mysql,inline"`
 }
 
-type driver struct {
+type dbdriver struct {
 	database database `yaml:"database,inline"`
 	driver string `yaml:"driver,omitempty"`
 	host string `yaml:"host,omitempty"`
@@ -21,11 +30,22 @@ type driver struct {
 	dbpass string `yaml:"pass,omitempty"`
 	port int `port:"port,omitempty"`
 }
-var dbdriver driver
-func loadSettings() {
+func (d dbdriver) loadSettings() (dbdriver) {
+	// slurping the config.yml file into memory.  and allowing the yaml framework handle the data read
+	// This should get all setings from the file.
 	dat, err := ioutil.ReadFile("../config.yml")
-	yaml.Unmarshal(dat, &dbdriver)
+	yaml.Unmarshal(dat, &d)
 	if err != nil {
 		log.Fatal("cannot unmarshal data %v", err)
 	}
+	return d
+}
+
+func (web webSettings) loadSettings() (webSettings) {
+	dat, err := ioutil.ReadFile("../config.yml")
+	yaml.Unmarshal(dat,&web)
+	if err != nil {
+		log.Fatal("cannot unmarshal data %v" ,err)
+	}
+	return web
 }
