73110e51ec8d22f7e207cb0c8669e00e96acb72a
Cameron Pickle <cmpickle@gmail.com>
2017-11-08 18:35:52 -0700

    update inventory routes

--- routes/inventory.go
+++ routes/inventory.go
@@ -2,7 +2,23 @@ package routes
 
 import "net/http"
 
+func getInventories(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func getInventory(w http.ResponseWriter, r *http.Request) {
+
+}
+
 // Updates the inventory value for the inventory item
 func updateInventory(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func incrementInventory(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func decrementInventory(w http.ResponseWriter, r *http.Request) {
 
 }
