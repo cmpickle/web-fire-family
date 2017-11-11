a6a45d78d2d0c4fd0be911e0b2515a8ab30c1d87
Amarantz <se0022@hotmail.com>
2017-11-11 06:27:38 -0700

    Fixed the settings file as well create test both for db settings and web settings.

--- app/settings.go
+++ app/settings.go
@@ -9,26 +9,17 @@ import (
 	//"os"
 )
 
-type web  struct {
-	y string `yaml:"web,inline"`
-}
-
-type WebSettings struct {
-	port int `port:"port",omitempty`
-}
-
-type database struct{
-	// should move to the inline of the file. so we get whats under the mysql structure of the yaml file.
-	Y string `yaml:"mysql,inline"`
+type Web  struct {
+	Port int `yaml:"webport,omitempty"`
 }
 
 type Dbdriver struct {
-	Database database `yaml:"database,inline"`
+	Database string `yaml:"database,omitempty"`
 	Driver string `yaml:"driver,omitempty"`
 	Host string `yaml:"host,omitempty"`
 	Dbuser string `yaml:"user,omitempty"`
 	Dbpass string `yaml:"pass,omitempty"`
-	Port int `port:"port,omitempty"`
+	Port int `yaml:"dbport,omitempty"`
 }
 func (d Dbdriver) LoadSettingsDefault() (Dbdriver) {
 	// slurping the config.yml file into memory.  and allowing the yaml framework handle the data read
@@ -41,7 +32,7 @@ func (d Dbdriver) LoadSettingsDefault() (Dbdriver) {
 	return d
 }
 
-func (web WebSettings) loadSettings() (WebSettings) {
+func (web Web) loadSettingsDefault() (Web) {
 	dat, err := ioutil.ReadFile("../config.yml")
 	yaml.Unmarshal(dat,&web)
 	if err != nil {
@@ -50,13 +41,24 @@ func (web WebSettings) loadSettings() (WebSettings) {
 	return web
 }
 
-func (d Dbdriver) LoadSettings(s string) (DbDriver) {
-// slurping the config.yml file into memory.  and allowing the yaml framework handle the data read
-// This should get all setings from the file.
-dat, err := ioutil.ReadFile("../config.yml")
-yaml.Unmarshal(dat, &d)
-if err != nil {
-log.Fatal("cannot unmarshal data %v", err)
+func (d Dbdriver) LoadSettings(s string) (Dbdriver) {
+	// slurping the config.yml file into memory.  and allowing the yaml framework handle the data read
+	// This should get all setings from the file.
+	dat, err := ioutil.ReadFile(s)
+	yaml.Unmarshal(dat, &d)
+	if err != nil {
+		log.Fatal("cannot unmarshal data %v", err)
+	}
+	return d
 }
-return d
+
+func (web Web) LoadSettings(s string) (Web) {
+	// slurping the config.yml file into memory.  and allowing the yaml framework handle the data read
+	// This should get all setings from the file.
+	dat, err := ioutil.ReadFile(s)
+	yaml.Unmarshal(dat, &web)
+	if err != nil {
+		log.Fatal("cannot unmarshal data %v", err)
+	}
+	return web
 }
--- main.go
+++ main.go
@@ -3,18 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
-
+	"./app"
 	"./models"
 	"./routes"
+	"strconv"
 )
-
+var Dbdriver app.Dbdriver
+var web app.Web
 func main() {
-	db, err := models.InitDB()
+	Dbdriver = Dbdriver.LoadSettings("./config.yml")
+	web = web.LoadSettings("./config.yml")
+	var addr string
+	addr = ":" + strconv.Itoa(web.Port)
+	db, err := models.InitDB(&Dbdriver)
 	if err != nil {
 		fmt.Errorf("Database wasn't initialized!")
 	}
 
 	env := models.Env{Db: db}
 
-	http.ListenAndServe(":8000", routes.InitRoutes(env))
+	http.ListenAndServe(addr, routes.InitRoutes(env))
 }
