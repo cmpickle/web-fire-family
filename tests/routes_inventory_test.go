61f73a768b58bc6863903f62ca3716917b67b183
Cameron Pickle <cmpickle@gmail.com>
2017-11-17 18:40:56 -0700

    added TestGetInventory and TestGetInventoryInvalidID unit tests

--- tests/routes_inventory_test.go
+++ tests/routes_inventory_test.go
@@ -67,7 +67,7 @@ func TestGetInventories(t *testing.T) {
 
 func TestGetInventory(t *testing.T) {
 	// Create a request to pass to our handler. We don't have any query parameters for now so we'll pass 'nil' as the third parameter.
-	req, err := http.NewRequest("GET", "/product/1", nil)
+	req, err := http.NewRequest("GET", "/inventory/1", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -81,10 +81,10 @@ func TestGetInventory(t *testing.T) {
 	defer db.Close()
 
 	// before we actually execute our api function, we need to expect required DB actions
-	rows := sqlmock.NewRows([]string{"productid", "productname", "notificationquantity", "color", "trimcolor", "size", "price", "dimensions", "sku", "deleted"}).
-		AddRow(1, "Firefighter Wallet", 10, "Tan", "Black", "size", 30, "3 1/2\" tall and 4 1/2\" long", 1, 0)
+	rows := sqlmock.NewRows([]string{"inventoryid", "quantity", "datelastupdated", "productid", "deleted"}).
+		AddRow(1, 10, "11/17/2017", 1, 0)
 	mock.ExpectBegin()
-	mock.ExpectQuery("^SELECT (.+) FROM Product WHERE ProductID = \\?$").WillReturnRows(rows)
+	mock.ExpectQuery("^SELECT (.+) FROM Inventory WHERE InventoryID = \\?$").WillReturnRows(rows)
 	mock.ExpectCommit()
 
 	router := routes.InitRoutes(models.Env{db})
@@ -97,7 +97,7 @@ func TestGetInventory(t *testing.T) {
 	}
 
 	// Check the response body is what we expect.
-	expected := `[{"productid":1,"productname":"Firefighter Wallet","notificationquantity":10,"color":"Tan","trimcolor":"Black","size":"size","price":30,"dimensions":"3 1/2\" tall and 4 1/2\" long","sku":1}]`
+	expected := `[{"inventoryid":1,"quantity":10,"datelastupdated":"11/17/2017","productid":1}]`
 	equal, err := AreEqualJSON(w.Body.String(), expected)
 	if !equal {
 		t.Errorf("handler returned unexpected body: got %v want %v", w.Body.String(), expected)
@@ -111,7 +111,7 @@ func TestGetInventory(t *testing.T) {
 
 func TestGetInventoryInvalidID(t *testing.T) {
 	// Create a request to pass to our handler. We don't have any query parameters for now so we'll pass 'nil' as the third parameter.
-	req, err := http.NewRequest("GET", "/product/8000", nil)
+	req, err := http.NewRequest("GET", "/inventory/8000", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -125,7 +125,7 @@ func TestGetInventoryInvalidID(t *testing.T) {
 	defer db.Close()
 
 	mock.ExpectBegin()
-	mock.ExpectQuery("^SELECT (.+) FROM Product WHERE ProductID = \\?$").WillReturnError(fmt.Errorf("404 - Product not found"))
+	mock.ExpectQuery("^SELECT (.+) FROM Inventory WHERE InventoryID = \\?$").WillReturnError(fmt.Errorf("404 - Inventory not found"))
 
 	router := routes.InitRoutes(models.Env{db})
 
